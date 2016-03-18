a3b4b869af08f2c7a28cbadd2ab7611625db4260
CarloLucibello <carlo.lucibello@gmail.com>
2016-03-18 10:31:43 +0100

    fix bug in vertex removed

--- src/GraphVisualize.jl
+++ src/GraphVisualize.jl
@@ -48,7 +48,7 @@ function update!(plt)
         push!(plt.s_vertex_pos, push!(value(plt.s_vertex_pos), new_pos(res, psize)))
     elseif vertex_removed(g, glast)
         # Assume that the removed vertex was exchanged with last vertex
-        # This is an hack, and it is buggy.
+        # This is an hack, and it is slow and buggy.
         # In order to have a graphically consistent vertex removal,
         # we need either unique vertex identifiers or some external signaling.
 
@@ -56,7 +56,7 @@ function update!(plt)
         ilast = 0
         i = 1
         for neigs in fadj(g)
-            if neigs == lastneigs
+            if neigs == filter(x->x!=i,lastneigs)
                 neigs != neighbors(glast,i) && break
                 ilast = i
             end
